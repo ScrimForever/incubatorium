c24e255f484be29f16a17d1838447527e4637692
Thiago Salgado <thiago.salgado.monteiro@gmail.com>
2026-08-29 14:49:47 -0300

    Feat: Adicionando acesso ao banco de dados e setup de configuração de variáveis

---
+++ src/infra/db.py
@@ -0,0 +1,39 @@
+from collections.abc import AsyncGenerator
+
+from fastapi import Depends
+from fastapi_users.db import SQLAlchemyBaseUserTableUUID, SQLAlchemyUserDatabase
+from settings import settings
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class User(SQLAlchemyBaseUserTableUUID, Base):
+    pass
+
+
+engine: AsyncEngine = create_async_engine(str(settings.pg_dsn))
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def create_db_and_tables():
+    # noinspection PyTypeChecker
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession]:
+    async with async_session_maker() as session:
+        yield session
+
+
+async def get_user_db(session: AsyncSession = Depends(get_async_session)):
+    yield SQLAlchemyUserDatabase(session, User)
---
+++ src/infra/settings.py
@@ -0,0 +1,35 @@
+import os
+from pathlib import Path
+
+from pydantic import PostgresDsn, computed_field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+ENV = os.getenv("ENV", "development")
+env_file = f".env.{ENV}"
+
+if not Path(env_file).exists():
+    raise FileNotFoundError(f"Arquivo de configuração '{env_file}' não encontrado.")
+
+
+class Settings(BaseSettings):
+    pg_user: str = "postgres"
+    pg_password: str = ""
+    pg_host: str = "localhost"
+    pg_port: int = 5432
+    pg_database_name: str = "tecincubadora"
+    secret_key: str = ""
+
+    model_config = SettingsConfigDict(
+        env_file=env_file if ENV == "development" else None,
+        env_file_encoding="utf-8",
+    )
+
+    @computed_field
+    @property
+    def pg_dsn(self) -> PostgresDsn:
+        return PostgresDsn(
+            f"postgresql+asyncpg://{self.pg_user}:{self.pg_password}@{self.pg_host}:{self.pg_port}/{self.pg_database_name}"
+        )
+
+
+settings = Settings()
